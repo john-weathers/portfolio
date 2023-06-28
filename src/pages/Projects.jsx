9095551825375d320cf5ec4c2439417b768d1fd7
John Weathers <weathers.johnb@gmail.com>
2023-06-28 14:15:22 -0500

    Visibility on description, add OutletContext

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -11,9 +12,10 @@ const projects = {
 
 const Projects = () => {
   const [project, setProject] = useState([]);
+  const { pageHeight } = useOutletContext();
 
   return (
-    <div className='projects'>
+    <div className='projects' style={{ height: pageHeight }}>
       <div className='container'>
         <h1 className='highlight'>Projects by me.</h1>
         <div className='flex-container'>
@@ -30,23 +32,16 @@ const Projects = () => {
             <button onClick={() => setProject(projects.comingSoon)} className={project[2] === '/comingSoon' ? 'clicked destyled-btn' : 'destyled-btn'} >coming soon...</button>
           </div>
         </div>
-        {project?.length > 0 && (
-          <div className='description'>
-            {/*Array.isArray(project[0]) ? (
-              <h2>{project[0][0]}<span>{project[0][1]}</span></h2>
-            ) : (
-              <h2>{project[0]}</h2>
-            )*/}
-            <p>{project[1]}</p>
-            <a 
-              href={project[3] ? project[2] : null} 
-              target='_blank' 
-              style={{ backgroundColor: project[3] ? project[3] : 'rgba(0, 0, 0, 0.2)', cursor: project[3] ? 'pointer' : 'default' }}
-            >
-              Go to project
-            </a>
-          </div>
-        )}
+        <div className={project?.length > 0 ? 'description show' : 'description hide'}>
+          <p>{project[1]}</p>
+          <a 
+            href={project[3] ? project[2] : null} 
+            target='_blank' 
+            style={{ backgroundColor: project[3] ? project[3] : 'rgba(0, 0, 0, 0.2)', cursor: project[3] ? 'pointer' : 'default' }}
+          >
+            Go to project
+          </a>
+        </div>
       </div>
     </div>
   )
