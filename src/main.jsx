dd63fb83fdef9916f9241ddf743e34f930bb2c44
John Weathers <weathers.johnb@gmail.com>
2023-07-01 13:56:08 -0500

    Preparation for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './App';
 import './index.css'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (import.meta.env.PROD) disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
